f40830bec84f3a2935eda04b8cbc8cf3f1d84155
alyssonjs <alysson.soares@hotmail.com.br>
2023-05-12 14:31:27 -0300

    - Create Phrase to count works

--- word_count.rb
+++ word_count.rb
@@ -1,8 +1,12 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
-
+class Phrase
+    attr_reader :text
+  
+    def initialize(text)
+      @text = text
+    end
+  
+    def word_count
+      words = text.downcase.scan(/\b[\w']+\b/)
+      words.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
+    end
+end
